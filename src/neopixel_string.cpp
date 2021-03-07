0eb90996f4471ff4709c48d19a338e56d1a586ce
Sven Dowideit <SvenDowideit@home.org.au>
2021-03-07 11:20:21 +1000

    just hack the old version works
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- src/neopixel_string.cpp
+++ src/neopixel_string.cpp
@@ -1,20 +1,25 @@
 #include "secrets.h"
 
-#ifdef ESP8266
-#include <servo.h>
+// #ifdef ESP8266
+// #include <servo.h>
 
-#include <neopixel.h>
-#include <motor.h>
-#include <relay.h>
-#include <switch.h>
+// #include <neopixel.h>
+// #include <motor.h>
+// #include <relay.h>
+// #include <switch.h>
 
-#else
-//esp32 only
-#include <touch.h>
-#endif
+// #else
+// //esp32 only
+// #include <touch.h>
+// #endif
 
-#include <web.h>
-#include <mqtt.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Homie.h>
+
+#include "ButtonNode.hpp"
+
+ButtonNode buttonNode("button", "test", D3);
 
 //
 //breif:
@@ -28,60 +33,23 @@
 
 // GO READ https://www.tweaking4all.com/hardware/arduino/adruino-led-strip-effects/
 
-//BUILD with "LOLIN(WEMOS) D1 R2 & mini"
-Mqtt mqtt = Mqtt(SECRET_SSID, SECRET_PASSWORD, "mqtt", 1883, "multipass");
-
-Device *devices[5];
-int deviceCount = 0;
-
 // #define FASTLED_ESP8266_RAW_PIN_ORDER
 // #define FASTLED_ALLOW_INTERRUPTS 0
 // #include <FastLED.h>
 #define NUM_LEDS 50
-CRGB leds[NUM_LEDS];
+//CRGB leds[NUM_LEDS];
 
-void mqtt_callback_fn(const char *topic, const byte *raw_payload, unsigned int length)
-{
-  Serial.printf("Callback: %s\r\n", topic);
-
-  char payload[256];
-  strncpy(payload, (char *)raw_payload, length);
-  payload[length] = 0;
-  Serial.printf("Callback: %s: %s\r\n", topic, payload);
-
-  if (strncmp("all/", topic, 4) == 0)
-  {
-    topic = topic + 4;
-  }
-  else
-  {
-    const char *hostname = mqtt.getHostname();
-    size_t length = strlen(hostname);
-    if (strncmp(hostname, topic, length) == 0)
-    {
-      topic = topic + length + 1;
-    }
-    else
-    {
-      Serial.printf("ERROR: topic(%s) didn't start with all/ or %s\r\n", topic, hostname);
-    }
-  }
-  // THIS NEEDS TO FIGURE OUT THE RIGHT DEVICE TO call
-  // AND send to multiple for "all"
-  for (int i = 0; i < deviceCount; i++)
-  {
-    const char *message = devices[i]->IsMessageForMe(topic);
-    if (message != NULL)
-    {
-      devices[i]->mqtt_callback_fn(topic, payload, length);
-    }
-  }
-}
+#define FW_NAME "folly-engine-nodes"
+#define FW_VERSION "0.0.1"
 
 void setup()
 {
+  // https://homieiot.github.io/homie-esp8266/docs/3.0.1/configuration/json-configuration-file/
+  Homie_setFirmware(FW_NAME, FW_VERSION);
+  Homie_setBrand("Folly");
+
   //while (!Serial);
-  Serial.begin(115200);
+  Serial.begin(9600); //115200
   int startDelay = random(100) * 100;
   Serial.printf("Started, delaying %d mS\r\n", startDelay);
   delay(startDelay);
@@ -115,49 +83,25 @@ void setup()
   // use D3 and D6
   //devices[deviceCount++] = new ServoDevice(&mqtt);
   // use D3
-  devices[deviceCount++] = new SwitchDevice(&mqtt);
+  // devices[deviceCount++] = new SwitchDevice(&mqtt);
   // use D1
   //devices[deviceCount++] = new RelayDevice(&mqtt);
 #else
   devices[deviceCount++] = new TouchDevice(&mqtt); //<<<<<<<<<------ for the esp32
 #endif
 
-  mqtt.setup();
-  Serial.printf("All set up\r\n");
-  web_setup(SECRET_PASSWORD);
-  Serial.printf("web set up\r\n");
+  //buttonNode.beforeHomieSetup();
 
-  //devices[0]->setup();
-  for (int i = 0; i < deviceCount; i++)
-  {
-    devices[i]->setup();
-    devices[i]->subscribe();
-  }
-  Serial.printf("subscribe set up\r\n");
+  Homie.disableLedFeedback();
+  Homie.disableResetTrigger();
+
+  Homie.setup();
 
   // TODO: need to redo the callback thing so there's one callback per subscription that then gets the pre-parsed json
-  mqtt.setCallback(mqtt_callback_fn);
-  Serial.printf("callback set up\r\n");
+  Serial.printf("started\r\n");
 }
 
 void loop()
 {
-  if (!mqtt.loop())
-  {
-    // if we had to reinit, delay a little
-    delay(100);
-
-    for (int i = 0; i < deviceCount; i++)
-    {
-      devices[i]->subscribe();
-    }
-    return;
-  }
-
-  // devices[0]->loop();
-  for (int i = 0; i < deviceCount; i++)
-  {
-    devices[i]->loop();
-  }
-  web_loop();
+  Homie.loop();
 }
